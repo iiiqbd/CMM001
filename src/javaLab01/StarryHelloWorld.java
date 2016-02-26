4b44c4d7d2f0a851c9493dc43b03c56638598964
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-02-26 10:19:22 +0000

    migrating all java code to unified cmm001 folder

---
+++ src/javaLab01/StarryHelloWorld.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab01;
+
+/**
+ *
+ * @author Uche
+ */
+public class StarryHelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("*******************");
+        System.out.println("*** Hello World ***");
+        System.out.println("*******************");
+    }
+}
+
+/*
+    System.out.print("*******************\n");
+    System.out.print("*** Hello World ***\n");
+    System.out.print("*******************\n");
+*/
+
+/*
+    System.out.printf("*******************\n");
+    System.out.printf("*** Hello World ***\n");
+    System.out.printf("*******************\n");
+*/
