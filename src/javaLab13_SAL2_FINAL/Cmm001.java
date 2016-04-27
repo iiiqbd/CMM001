971f5cd483e0199a8ac50fd7783ee38eb03f90e4
iiiqbd <u.i.adiele@rgu.ac.uk>
2016-04-27 17:13:44 +0100

    all solns finally here!

---
+++ src/javaLab13_SAL2_FINAL/Cmm001.java
@@ -0,0 +1,245 @@
+package javaLab13_SAL2_FINAL;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ * Uses Cashier, Fuel and Sale classes to create a Service Station application
+ * @author David Lonie
+ */
+public class Cmm001 {
+
+    public static void main(String[] args) {
+
+    // Create objects representing cashiers at filing station
+        // Create an ArrayList of Cashiers to represent today's cashiers
+        ArrayList<Cashier> cashiers = new ArrayList<>();
+        // Create a Cashier to represent manager
+        Cashier manager = new Cashier("manager","1234");
+        // a Cashier object to represent cashier currently processing fuel sales
+        Cashier currentcashier;
+        // Add manager to list of today's cashiers
+        cashiers.add(manager);
+        // Set manager as the currentcashier
+        currentcashier = manager;//
+
+    // Create objects reprsenting fuels sold by filling station
+        ArrayList<Fuel> fuels = new ArrayList<>();
+        Fuel petrol = new Fuel("Petrol", 1.145, 40000, 50000);
+        Fuel diesel = new Fuel("Diesel", 1.196, 45000, 50000);
+        Fuel hydrogen = new Fuel("Hydrogen", 0.528, 15000, 20000);
+        fuels.add(petrol);
+        fuels.add(diesel);
+        fuels.add(hydrogen);
+        Fuel currentfuel;
+
+        String fuellist ="" ;
+        for(int i = 0; i < fuels.size(); i++){
+            fuellist += i + "  " + fuels.get(i).getName() + "\n";
+        }
+
+    // Create objects representing fuel Sales made today
+        ArrayList<Sale> todaysSales = new ArrayList<>();
+
+    // create DecimalFormat object for displaying cash values to nearest £
+        DecimalFormat to2dp = new DecimalFormat("0.00");
+
+    // Ceate variables used by menu for user input
+
+        String input; // local variable to hold user responses from menu
+        char choice; // local variable on which to base switch cases
+
+    // create user menu to interact with Order called order
+        do{ String menutext = "Cashier logged in is: "
+                + currentcashier.getName()
+                + "\n\nPlease select an option [1]-[8] or [0] to exit:\n\n"
+                + "[1] Process a sale of Fuel\n"
+                + "[2] Check Fuel stocks\n"
+                + "[3] Refill Fuel storage tank\n"
+                + "[4] Display sales of " + currentcashier.getName() + "\n"
+                + "[5] Log in another cashier\n"
+                + "[6] Update Fuel prices\n"
+                + "[7] Summarise sales by Fuel\n"
+                + "[8] Summarise sales by Cashier\n\n"
+                + "[0] Exit ";
+            input = JOptionPane.showInputDialog(menutext);
+            choice = input.charAt(0);
+
+            switch(choice){
+
+                case '1': {// code for processing a sale of fuel
+                    int fuelchoice;
+                    do{
+                        fuelchoice = Integer.parseInt(JOptionPane.showInputDialog(
+                                "What type of Fuel? Enter [0]-[2]:\n" + fuellist));
+                    }while(fuelchoice < 0 || fuelchoice >= fuels.size());
+                    Fuel fuel = fuels.get(fuelchoice);
+                    double l = Double.parseDouble(JOptionPane.showInputDialog(
+                            "Enter number of litres of " + fuel.getName()));
+                    boolean d = JOptionPane.showInputDialog(
+                            "Apply discount? [Enter y if yes]:").equals("y");
+                    Sale sale = new Sale(fuel, l, d, currentcashier);
+                    if(fuel.getStock()<l){
+                        JOptionPane.showMessageDialog(null, "Sorry insufficient "
+                                + fuel.getName() + " in stock, only "
+                                + sale.getLitres() + " available");
+                    }
+                    todaysSales.add(sale);
+                    currentcashier.processSale(sale);
+                    fuel.pumpFuel(l);
+
+                    JOptionPane.showMessageDialog(null, sale.getLitres()
+                            + " litres of " + fuel.getName()
+                            + " £" + to2dp.format(sale.getCost())  );
+                    break;
+                }
+
+                case '2': {// code for checking fuel stocks
+                    String message = "";
+                    for(int i = 0; i < fuels.size(); i++){
+                        message += fuels.get(i).toString() + "\n";
+                        //System.out.println(fuels.get(i).toString());
+                    }
+                    JOptionPane.showMessageDialog(null, message);
+                    break;
+                }
+
+                case '3': { // code for adding fuel to storage tank
+                    int fuelchoice;
+                    do{
+                        fuelchoice = Integer.parseInt(JOptionPane.showInputDialog(
+                                "What type of Fuel is being refilled. "
+                                        + "Enter [0]-[2]\n"
+                                        + fuellist));
+                    }while(fuelchoice < 0 || fuelchoice >= fuels.size());
+                    Fuel fuel = fuels.get(fuelchoice);
+                    fuel.refill();
+                    JOptionPane.showMessageDialog(null, fuel.getName()
+                            + " stock refilled to "
+                            + fuel.getCAPACITY() + " litres");
+                    break;
+                }
+
+                case '4': { // code for displaying fuel sales of current cashier
+                    String message = currentcashier.getName()
+                            + " fuel sales on current shift:\n"
+                            + currentcashier.getNumberSales()
+                            + " sales of total value £"
+                            + to2dp.format(currentcashier.getTakings()) + "\n";
+                    for(Sale s: Sale.byCashier(currentcashier, todaysSales)){
+                        message += s.getLitres() + " litres of "
+                                + s.getFuel().getName() + " £"
+                                + to2dp.format(s.getCost())+ "\n";
+
+                    }
+                    System.out.println(message);
+                    JOptionPane.showMessageDialog(null, message );
+                    break;
+                }
+
+                case '5':{ // code for new Cashier log in
+                    String name = JOptionPane.showInputDialog(
+                            "Please enter your name: ");
+                    String id = JOptionPane.showInputDialog(
+                            "Please enter your staff ID:\n");
+                    boolean isNew = true;
+                    for(Cashier c: cashiers){
+                        if(c.getName().equals(name)){
+                            isNew = false;
+                            if(c.checkID(id)){
+                                currentcashier = c;
+                            }
+                            else JOptionPane.showMessageDialog(null,
+                                    "Sorry, incorrect id for cashier "
+                                    + c.getName());
+                            break;
+                        }
+                    }
+                    if(isNew){
+                        Cashier newCashier = new Cashier(name, id);
+                        cashiers.add(newCashier);
+                        currentcashier = newCashier;
+                    }
+                }
+                break;
+
+                case '6':{ // code for updating fuel prices
+                    int fuelchoice;
+                    do{
+                        fuelchoice = Integer.parseInt(JOptionPane.showInputDialog(
+                                "What Fuel is being repriced. "
+                                        + "Enter [0]-[2]\n"
+                                        + fuellist));
+                    }while(fuelchoice < 0 || fuelchoice >= fuels.size());
+                    Fuel fuel = fuels.get(fuelchoice);
+                    double ppl = Double.parseDouble(JOptionPane.showInputDialog(
+                                "New cost of " + fuels.get(fuelchoice).getName()));
+                    fuel.setPrice(ppl);
+                    JOptionPane.showMessageDialog(null, "Cost of "
+                            + fuel.getName()  + " now £"
+                            + fuel.getPrice());
+                }
+                break;
+
+                case '7': {// code for summarising sales by fuel type
+                    String message = "Summary of sales, listed by Fuel type\n";
+                    message += Sale.getTotalNumber() + " sales, totalling £"
+                            + to2dp.format(Sale.getTotalValue()) + "\n";
+                    for(Fuel f: fuels){
+                        message += f.getName() + ":\n";
+                        ArrayList<Sale> fSale = Sale.ofFuel(f, todaysSales);
+                        for(Sale s: fSale){
+                            message += s.getLitres() + " litres, cashier: "
+                                    + s.getStaff().getName() + "\n";
+                        }
+                    }
+                    System.out.println(message);
+                    JOptionPane.showMessageDialog(null, message);
+                    break;
+                }
+
+                case '8': {// code for summarising sales by cashier
+                    String message = "Summary of sales, listed by Cashier\n";
+                    message += Sale.getTotalNumber() + " sales, totalling £"
+                            + to2dp.format(Sale.getTotalValue()) + "\n";
+                    for(Cashier c: cashiers){
+                        message += c.getName() + ":"
+                                + " number of sales: " + c.getNumberSales()
+                                + " value of sales £" + to2dp.format(c.getTakings()) +"\n";
+                        ArrayList<Sale> fSale = Sale.byCashier(c, todaysSales);
+                        for(Sale s: fSale){
+                            message += s.getLitres() + " litres of "
+                                    + s.getFuel().getName() + "\n";
+                        }
+                    }
+                    System.out.println(message);
+                    JOptionPane.showMessageDialog(null, message);
+                    break;
+                }
+
+                case '0': // code executed when leaving the do-while statement
+                {   String message = "Closing Sales Point \n"
+                        + "Have a nice day!";
+                    JOptionPane.showMessageDialog(null, message);
+                    break;
+                }
+
+                default:
+                {    JOptionPane.showMessageDialog(null,
+                            "Sorry, choice not recognised.");
+                }
+            } // end of switch
+        }while(choice!='0');
+    }
+
+    private static String printSales(ArrayList<Sale> in){
+        String out = "";
+        for(Sale s: in){
+            out+=s.toString() + "\n";
+            System.out.println(s.toString());
+        }
+        return out;
+    }
+
+}
---
+++ src/javaLab13_SAL2_FINAL/Sale.java
@@ -0,0 +1,152 @@
+package javaLab13_SAL2_FINAL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+ /**
+ * Class encapsulating the details of a Sale of Fuel by a Cashier
+ * uses Fuel and Cashier classes
+ * @author David Lonie
+ */
+public class Sale {
+    private Fuel fueltype;          // represents the type of Fuel being sold   
+    private double litres;          // represents the volume of Fuel being sold
+    private boolean discounted;     // represents whether customer is is a member of discount loyalty scheme
+    private Cashier staff;          // represents the cashier who is processing the Sale of Fuel
+
+  /**
+ * @value totalValue     class variable representing total value of all Sales
+ */
+    private static double totalValue = 0.0; 
+  /**
+ * @value totalNumber     class variable representing total number of all Sales
+ */
+    private static int totalNumber = 0;
+
+  /**
+ * Constructor for Customer object, takes 4 parameters.
+ * @param f    Fuel object representing the type of fuel being purchased,
+ * @param l    double value representing the number of litres of fuel being purchased, 
+ * @param d    boolean value representing whether this Sale is discounted,
+ * @param c    Cashier object representing the member of staff processing the Sale
+ */
+    public Sale(Fuel f, double l, boolean d, Cashier c){
+        fueltype = f;
+        discounted = d;
+        staff = c;
+        if(fueltype.getStock()<l){
+            litres = fueltype.getStock();   
+        }
+        else litres = l;
+        totalNumber ++;
+        totalValue += getCost(); 
+    }
+
+  /**
+ * object method returning a Fuel object associated with this Sale
+ * @return      Fuel object reference to fueltype property of this Sale
+ */ 
+    public Fuel getFuel(){
+        return fueltype;
+    }
+
+  /**
+ * object method returning the number of litres of fuel associated with this Sale
+ * @return      double value equal to the litres property of this Sale
+ */    
+    public double getLitres(){
+        return litres;
+    }
+
+  /**
+ * object method returning the Cashier object that facilitated this Sale
+ * @return      boolean value equal to the discounted property of this Sale
+ */    
+    public boolean isDiscounted(){
+        return discounted;
+    }
+ 
+  /**
+ * object method returning the Cashier object that processed this Sale
+ * @return      Cashier object reference to staff property of this Sale
+ */    
+    public Cashier getStaff(){
+        return staff;
+    }
+    
+ /**
+ * object method returning cost of a fuel sale 
+ * @return      double value equal to cost of specified litres of fueltype, 
+ * taking into account whether loyalty discount is applicable or not
+ */
+    public double getCost(){
+        double c;
+        if(discounted) c = fueltype.getPrice()*litres*(1-Fuel.getDiscount());
+        else c = fueltype.getPrice()*litres;
+        return c;
+    }
+   
+ /**
+ * object method that returns a String object summarising the properties of a Sale object  
+ * @return     a String: in the format e.g.
+ *             40 litres of fueltype  cashier: cashiername, or
+ *             40 litres of fueltype  (with 2% discount) cashier: cashiername
+ */
+    @Override
+    public String toString(){
+        String message = litres + " litres of " 
+                + fueltype.getName();
+        if(discounted) message += " (with " + (Fuel.getDiscount()*100) + "% discount)";
+        message += ", cashier: " + staff.getName();
+        return message;
+    }
+    
+ /**
+ * class-level method for accessing the class-level totalValue property 
+ * that represents the combined value of sales made by all Cashiers
+ * @return     double value equal to the class-level variable totalValue
+ */
+  public static double getTotalValue(){
+       return totalValue;
+  }
+   
+ /**
+ * class-level method for accessing the class-level totalNumber property 
+ * that represents the combined number of sales made by all Cashiers
+ * @return      double value equal discounted cost of l litres of this fuel
+ */
+  public static int getTotalNumber(){
+       return totalNumber;
+  }
+  
+ /**
+ * class-level method that filters List of Sales and returns those 
+ * associated with a specified Cashier
+ * @return  List of Sale objects
+ * @param   c  a Cashier object reference
+ * @param   tList List of Sale objects
+ */
+  public static ArrayList<Sale> byCashier(Cashier c, ArrayList<Sale> tList){
+      ArrayList<Sale> cList = new ArrayList<>();
+      for(Sale t: tList){
+          if(t.getStaff().equals(c)) cList.add(t);
+      }
+      return cList;
+  } 
+ 
+  
+ /**
+ * class-level method that filters List of Sales and returns those 
+ * associated with a specified Fuel type
+ * @return  List of Sale objects
+ * @param   f  a Fuel object reference
+ * @param   tList List of Sale objects
+ */
+  public static ArrayList<Sale> ofFuel(Fuel f, ArrayList<Sale> tList){
+      ArrayList<Sale> fList = new ArrayList<>();
+      for(Sale t: tList){
+          if(t.getFuel().equals(f)) fList.add(t);
+      }
+      return fList;
+  }
+} // end of Sale.class
